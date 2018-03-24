a2d6b375e119d781d379ad6538ba3b1ceb1d4522
pasface <pasiarah@gmail.com>
2018-03-24 18:32:11 -0400

    added a second effect that also doesn't do much good

--- src/components/Renderer/Renderer.js
+++ src/components/Renderer/Renderer.js
@@ -85,9 +85,14 @@ export default class Renderer extends React.Component {
     
     // create lights
     this.scene.add(new THREE.HemisphereLight(hemisphere_sky_color, hemisphere_ground_color, hemisphere_intensity));
+    
     // addShadowedLight parameters (x, y, z, color, intensity)
     this.addShadowedLight(1, 1, 1, 0xffffff, .8);
     this.addShadowedLight(0.5, 1, -1, 0xffffff, 1);
+    
+    // add ambientLight
+    const ambientLight = new THREE.AmbientLight(0x2c3e50);
+    this.scene.add(ambientLight);
     
     // create renderer
     this.renderer = new THREE.WebGLRenderer({antialias: true});
@@ -121,17 +126,53 @@ export default class Renderer extends React.Component {
   //handrail mouseover state
   handleHandrailMouseOver(e) {
     this.setState({hoveredHandrail: e.target});
-  }
+  };
+  
+  //create glow material
+  buildGlow(hexColor, vert, frag){
+	    // create glass material 
+	    // based on Lee Stemkoski's glow
+	    const material = new THREE.ShaderMaterial( 
+	   	{
+	   		uniforms: 
+	   		{ 
+	   			"c":   { type: "f", value: .1 },
+	   			"p":   { type: "f", value: 10 },
+	   			glowColor: { type: "c", value: new THREE.Color(hexColor) },
+	   			viewVector: { type: "v3", value: this.camera.position }
+	   		},
+	   		//add shaders to the material
+	   		vertexShader: vert,
+	   		fragmentShader: frag,
+	   		//set cast shadow to front
+	   		side: THREE.BackSide,
+	   		//set blending equation to blend RGB and Alpha
+	   		blending: THREE.AdditiveBlending,
+	   		//turn on transparency
+	   		transparent: true
+	  }   );
+	  
+	  //create explosion material
+/*	  const material = new THREE.RawShaderMaterial({
+		  vertexShader: vert,
+		  fragmentShader: frag,
+		  uniforms: {
+			  time: { type: 'f', value: 0 }
+		  },
+	  });*/
+	  return material;
+};
   
   //process stl and str files
   processFiles() {
 	//create constant props
     const {
       // -- set hand-rail color values here --
-      hr_color = '#3f5056', //blue-gray
-      hr_start_color = '#0fff00', // green //
-      hr_end_color = '#ff0022', //red
-      
+      hrColor = '#3f5056', //blue-gray
+      hrStartColor = '#0cff00', // green
+      hrEndColor = '#ff0022', //red
+      hrStartHexColor = 0x0cff00,
+      hrEndHexColor = 0xff0022,
       stationFile,
       handrailFiles,
       strFiles,
@@ -151,62 +192,16 @@ export default class Renderer extends React.Component {
         this.stationModel.material.dispose();
         this.stationModel = undefined;
       }
-      
 
-/*  	var sphereGeometry = new THREE.SphereGeometry( .3, 10, 10 );
-  	
-  	var lavaTexture = new THREE.TextureLoader.load( 'images/lava.jpg' );
-    var sphereMaterial = new THREE.MeshPhongMaterial( { map: lavaTexture } );
-	var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-	sphere.position.set(0,0,1);
-	this.scene.add(sphere);
-      
-    //create texture  
-	var lavaTexture = new THREE.TextureLoader().load( "images/lava.jpg" );
-	//create geometry
-	var geometry = new THREE.BoxBufferGeometry( .3, .3, .3);
-	//map texture to geometry
-	var sphereMaterial = new THREE.MeshBasicMaterial( { map: lavaTexture } );
-	//combine geometry and material into a mesh
-	var sphere = new THREE.Mesh( geometry, sphereMaterial );
-	//position mesh
-	sphere.position.set(0,0,1);
-	//add mesh to screen
-	this.scene.add( sphere );
-      
-      
-      //make sphere (radius, segmentsWidth, segmentsHeight)
-      var sphereGeom = new THREE.SphereGeometry(.5, 10, 10);
-
-      
-     
-      
-      //load texture
-      var lavaTexture = THREE.ImageUtils.loadTexture( 'images/lava.jpg' );
-      lavaTexture.wrapS = lavaTexture.wrapT = THREE.RepeatWrapping;
-      lavaTexture.repeat.set( 10, 10 );
-      var lavaMaterial = new THREE.MeshBasicMaterial( { map: lavaTexture } );
-      var lavaBall = new THREE.Mesh( sphereGeom, lavaMaterial );
-
-      //set position
-      lavaBall.position.set(0,0,-1);
-      //add to scene
-      this.scene.add(lavaBall);
-      
-      
-      */
-      
-      
-      
       const mesh = loadMeshFromFile(stationFile);
       this.stationModel = mesh;
       this.scene.add(mesh);
       this.camera.lookAt(mesh);
       this.stationModelIsDirty = false;
     }
-    
-    //script that defines the vertex shader
-    var vertexShaderSource = `
+
+    //script that defines the vertex shader for glass effect
+    const vertexShaderSource = `
     	uniform vec3 viewVector;
     	uniform float c;
     	uniform float p;
@@ -220,8 +215,8 @@ export default class Renderer extends React.Component {
     	}
     `;
     
-    //script that defines the fragment shader
-    var fragmentShaderSource = `
+    //script that defines the fragment shader for glass effect
+    const fragmentShaderSource = `
     	uniform vec3 glowColor;
     	varying float intensity;
     	void main() 
@@ -231,49 +226,115 @@ export default class Renderer extends React.Component {
     	}
     `;
     
-    // create glow material
-    const glowMaterial = new THREE.ShaderMaterial( 
-    	{
-    		uniforms: 
-    		{ 
-    			"c":   { type: "f", value: 1 },
-    			"p":   { type: "f", value: 3 },
-    			glowColor: { type: "c", value: new THREE.Color(0xff0022) },
-    			viewVector: { type: "v3", value: this.camera.position }
-    		},
-    		//add shaders to the material
-    		vertexShader: vertexShaderSource,
-    		fragmentShader: fragmentShaderSource,
-    		//set cast shadow to front
-    		side: THREE.FrontSide,
-    		//set blending equation to blend RGB and Alpha
-    		blending: THREE.AdditiveBlending,
-    		//turn on transparency
-    		transparent: true
-   	}   );
+/*  //script that defines the vertex shader for explosion effect 
+ *  based on Matt DesLauriers's bunny tutorial 
+    const vertexShaderSource = `
+    	attribute vec4 position;
+    	attribute vec3 normal;
+
+    	uniform mat4 projectionMatrix;
+    	uniform mat4 modelViewMatrix;
+
+    	varying vec3 vNormal;
+
+    	void main () {
+	    	vNormal = normal;
+	
+	    	vec4 offset = position;
+	    	float dist = 0.25;
+	    	offset.xyz += normal * dist;
+	    	gl_Position = projectionMatrix * modelViewMatrix * offset;
+    	}
+    `;
     
-    //load handrails
+    //script that defines the fragment shader for explosion effect
+    const fragmentShaderSource = `
+    	precision highp float;
+
+    	varying vec3 vNormal;
+
+    	void main () {
+    		gl_FragColor = vec4(vNormal, 1.0);
+    	}
+    `;*/
+
+    
     if (handrailFiles && Object.keys(handrailFiles).length > 0 && strFiles && strFiles.length > 0 ) {
     	Object.values(this.handrailModels).forEach(model => this.scene.remove(model));
     	Object.entries(handrailFiles).forEach(([name, handrailFile]) => {
-    		let color = hr_color;
+    		let color = hrColor;
     		let scale = 1;
     		var handrailMesh = loadMeshFromFile(handrailFile, {color}, {scale});
+    		
     		// set start/end/route handrail color
     		if (startHandrail && name === `${startHandrail.value}.stl`) {
-    			color = hr_start_color;
+    			//set color of handrail
+    			color = hrStartColor;
+    			
+    			//create handrail
     			handrailMesh = loadMeshFromFile(handrailFile, {color}, {scale});
+ 			
+    			// set glow material values for start handrail
+    			const glowMaterialStart = this.buildGlow(hrStartColor, vertexShaderSource, fragmentShaderSource);
+    			
     			// add glowy-handrail mesh to scene
-    			const handrailMeshClone = new THREE.Mesh(handrailMesh.geometry, new THREE.ShaderMaterial(glowMaterial));
-    			handrailMeshClone.scale.multiplyScalar(1.4);
+    			//how it's done for explosion material
+    			//const handrailMeshClone = new THREE.Mesh(handrailMesh.geometry, glowMaterialStart);
+    			
+    			// how it's done for glass effect material
+    			const handrailMeshClone = new THREE.Mesh(handrailMesh.geometry, glowMaterialStart);
+    			
+    			//scale up clone
+    			handrailMeshClone.scale.multiplyScalar(1.2);
+    			
+    			//add clone to scene in handrail location 
     			handrailMesh.add(handrailMeshClone);
+    			
+    			// add another glowy-handrail mesh to scene
+    			const handrailMeshClone2 = new THREE.Mesh(handrailMesh.geometry,glowMaterialStart);
+    			
+    			// scale it down
+    			handrailMeshClone2.scale.multiplyScalar(.8);
+    			
+    			//add to scene
+    			handrailMesh.add(handrailMeshClone2);
+    			
+    			//give it it's own light
+    			var light = new THREE.PointLight( hrStartHexColor, .5, .3 );
+    		    handrailMesh.add(light);
     		} else if (endHandrail && name === `${endHandrail.value}.stl`) {
-    			color = hr_end_color;
+    			// set color of handrail
+    			color = hrEndColor;
+    			// create handrail 
     			handrailMesh = loadMeshFromFile(handrailFile, {color}, {scale});
+    			
+    			// set glow material values for end handrail
+    			const glowMaterialEnd = this.buildGlow(hrEndColor, vertexShaderSource, fragmentShaderSource);
+    			
     			// add glowy-handrail mesh to scene
-    			const handrailMeshClone = new THREE.Mesh(handrailMesh.geometry, new THREE.ShaderMaterial(glowMaterial));
-    			handrailMeshClone.scale.multiplyScalar(1.4);
+    			//how it's done for explosion material
+    			//const handrailMeshClone = new THREE.Mesh(handrailMesh.geometry, glowMaterialEnd);
+    			
+    			// how it's done for glass effect material
+    			const handrailMeshClone = new THREE.Mesh(handrailMesh.geometry, glowMaterialEnd);
+    			
+    			// scale up the clone
+    			handrailMeshClone.scale.multiplyScalar(1.2);
+    			
+    			// add clone to scene in same location as handrail
     			handrailMesh.add(handrailMeshClone);
+    			
+    			// add another glowy-handrail mesh to scene
+    			const handrailMeshClone2 = new THREE.Mesh(handrailMesh.geometry,glowMaterialEnd);
+    			
+    			// scale it down
+    			handrailMeshClone2.scale.multiplyScalar(.8);
+    			
+    			//add to scene
+    			handrailMesh.add(handrailMeshClone2);
+    			//give it it's own light
+    			light = new THREE.PointLight( hrEndHexColor, .5, .4 );
+    		    handrailMesh.add(light);
     		} else {
     			// refactor and exit early or just loop routes outside for performance
     			routes.forEach(route => { 
@@ -296,7 +357,10 @@ export default class Renderer extends React.Component {
     }
     this.animate();
   } //end processFiles()
+  
+  
 
+  
   //shadowed light effect
   addShadowedLight(x, y, z, color, intensity) {
 	  const directionalLight = new THREE.DirectionalLight(color, intensity);
